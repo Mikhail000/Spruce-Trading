4027e0dcf479b168bbb80e93202d9047b0d57f02
Mikhail000 <57868799+Mikhail000@users.noreply.github.com>
2022-12-23 23:48:41 +0700

    smthhg

--- Assets/1 - Scripts/UI/SpruceButtonWidget.cs
+++ Assets/1 - Scripts/UI/SpruceButtonWidget.cs
@@ -13,44 +13,4 @@ public class SpruceButtonWidget : MonoBehaviour
     {
         buttonClick?.Invoke(spruceName);
     }
-
-    /*
-    public Image spruceWidgetImage;
-    public GameObject sprucePrefab;
-    public Transform parentWidgetPosition;
-
-    public override void Awake()
-    {
-        base.Awake();
-        parentWidgetPosition = transform.parent.transform;
-    }
-
-    public override void OnBeginDrag(PointerEventData eventData)
-    {
-        base.OnDrag(eventData);
-        spruceWidgetImage.enabled = false;
-        sprucePrefab.SetActive(true);
-        
-    }
-
-    public override void OnDrag(PointerEventData eventData)
-    {
-        base.OnDrag(eventData);
-        rectTransform.anchoredPosition += eventData.delta;
-    }
-
-    public override void OnEndDrag(PointerEventData eventData)
-    {
-        base.OnEndDrag(eventData);
-        transform.position = parentWidgetPosition.position;
-        spruceWidgetImage.enabled = true;
-        sprucePrefab.SetActive(false);
-    }
-
-    public override void OnPointerDown(PointerEventData eventData)
-    {
-        base.OnPointerDown(eventData);
-        
-    }
-    */
 }
